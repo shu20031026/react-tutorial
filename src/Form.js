431cfab1846f1834f550b1850ccfacd609d1b707
shu20031026 <shu20031026@gmail.com>
2021-01-17 22:27:34 +0900

    Data exchange (Function)

--- src/Form.js
+++ src/Form.js
@@ -1,3 +1,28 @@
-export const Form = () => {
-  return <div>フォーム</div>;
+import { useState } from "react";
+
+export const Form = ({ onAddLang }) => {
+  const [text, setText] = useState("");
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    onAddLang(text);
+  };
+
+  return (
+    <div>
+      <h4>新しい言語の追加</h4>
+      <form onSubmit={submitForm}>
+        <div>
+          <input
+            type="text"
+            vakue={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div>
+          <button>追加</button>
+        </div>
+      </form>
+    </div>
+  );
 };
